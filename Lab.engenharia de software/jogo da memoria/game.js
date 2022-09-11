595f91226aa418ee0e6f7cf8e5c0a94f2184133b
MarcoAntonio22-wq <81771434+MarcoAntonio22-wq@users.noreply.github.com>
2022-09-11 17:51:03 -0300

    versão final do jogo da memória

--- Lab.engenharia de software/jogo da memoria/game.js
+++ Lab.engenharia de software/jogo da memoria/game.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector(".grid");
+const timer = document.querySelector(".timer");
 
 const characters = [
   "castlevania",
@@ -19,6 +20,56 @@ const createElement = (tag, className) => {
   return element;
 };
 
+let firstCard = "";
+let secondCard = "";
+
+const checkEndGame = () => {
+  const disabledCards = document.querySelectorAll(".disabled-card");
+
+  if (disabledCards.length == 20) {
+    clearInterval(this.loop);
+    alert(`parabéns! você venceu! seu tempo foi de ${timer.innerHTML} segundos!`);
+  }
+};
+
+const checkCards = () => {
+  const firstcharacter = firstCard.getAttribute("data-character");
+  const secondcharacter = secondCard.getAttribute("data-character");
+
+  if (firstcharacter == secondcharacter) {
+    firstCard.firstChild.classList.add("disabled-card");
+    secondCard.firstChild.classList.add("disabled-card");
+
+    firstCard = "";
+    secondCard = "";
+
+    checkEndGame();
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove("reveal-card");
+      secondCard.classList.remove("reveal-card");
+
+      firstCard = "";
+      secondCard = "";
+    }, 750);
+  }
+};
+
+const revealCard = ({ target }) => {
+  if (target.parentNode.className.includes("reveal-card")) {
+    return;
+  }
+  if (firstCard == "") {
+    target.parentNode.classList.add("reveal-card");
+    firstCard = target.parentNode;
+  } else if (secondCard == "") {
+    target.parentNode.classList.add("reveal-card");
+    secondCard = target.parentNode;
+
+    checkCards();
+  }
+};
+
 const createCard = (character) => {
   const card = createElement("div", "card");
   const front = createElement("div", "face front");
@@ -28,6 +79,10 @@ const createCard = (character) => {
 
   card.appendChild(front);
   card.appendChild(back);
+
+  card.addEventListener("click", revealCard);
+
+  card.setAttribute("data-character", character);
 
   return card;
 };
@@ -43,4 +98,13 @@ const loadGame = () => {
   });
 };
 
-loadGame();
+const startTimer = () => {
+  this.loop = setInterval(() => {
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+  }, 1000);
+};
+window.onload = () => {
+  startTimer();
+  loadGame();
+};
